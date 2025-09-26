4974348328cf9b9a7fede086c8a940f4b3eccb75
osaidhaj <osaidhaj03@gmail.com>
2025-09-26 04:00:30 +0300

    refactor(admin): remove SystemOverviewWidget from panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -52,7 +52,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                SystemOverviewWidget::class,
+                //SystemOverviewWidget::class,
                 RecentActivitiesWidget::class,
                 SalesChartWidget::class,
                 PurchaseChartWidget::class,
